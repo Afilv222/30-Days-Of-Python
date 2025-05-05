db2846bd58c12d2ccc6028a57bcb2c87309deaa6
Afilv222 <55000122+Afilv222@users.noreply.github.com>
2025-05-05 13:20:21 -0600

    Finished doing all the exercises in day 2

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,6 +1,9 @@
 
 # Variables in Python
 
+import math
+
+
 first_name = 'Asabeneh'
 last_name = 'Yetayeh'
 country = 'Finland'
@@ -27,6 +30,9 @@ print('Age: ', age)
 print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
+print(len('Hello, World!'))
+print(type(person_info))
+
 
 # Declaring multiple variables in one line
 
@@ -38,3 +44,92 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+
+# Day 2: 30 Days of python programming
+
+first_name = 'Afil'
+last_name = 'Vidyasagar'
+full_name = first_name + ' ' + last_name
+country = 'Canada'
+city = 'Calgary'
+age = 25
+year_born = 2000
+is_married = 'Not yet'
+is_true = True
+is_light = False
+
+today_weather,season,temperature = 'Sunny','Spring',10
+
+num_one,num_two = 5,4
+
+print(type(first_name))
+print(f"First name length: {first_name} -> {len(first_name)}, last name length: {last_name} -> {len(last_name)}" )
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))    
+print(type(year_born))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light))
+print(type(today_weather))
+print(type(season))
+print(type(temperature))
+
+
+total = num_one + num_two 
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one**(num_two)
+floor_division = math.floor(division)
+
+
+print(total)
+print(diff)
+print(product)
+print(division)
+
+# how mod works is by taking the remainder of the numbers meaning, 4 % 5 is about 0.8 -> we floor the number meaning we round down 
+# then we take 5 x 0 which gives us 0. We need substract to get the remainder 4 - 0 = 4 so our answer is 4.
+
+# If we do 155 % 7 which is about 22.1428... then we take 22 and mutiple it by 7 which gives us 154, then we take 155 - 154 which is 1, the reminader is 1 
+
+#Similar to when we do 6 % 2 which is 3 then we take 3 and multiple it by 2 which gives us 6, then we take 6 - 6 which is zero 
+
+
+print(remainder)
+print(exp)
+print(floor_division)
+
+
+
+# Calculating radius of circle 
+
+
+radius = 0.3 
+
+def calculate_area(radius):
+    return math.pi * (radius)**2
+
+area_of_circle = calculate_area(radius)
+
+print(f'The area of a circle with radius 30: {area_of_circle}')
+
+def calculate_circumference(radius):
+    return 2 * math.pi * (radius)
+
+circum_of_circle = calculate_circumference(radius)
+
+print(f'The circumference of a circle with radius 30: {circum_of_circle}')
+
+custom_radius = float(input("Enter in Radius: "))
+
+print(f'The area of a circle with radius {custom_radius}: {calculate_area(custom_radius)}')
+
+
+
+
